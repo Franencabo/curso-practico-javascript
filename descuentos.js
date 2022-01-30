58e9d5441290a73f86ce082878c6b41a48eb9694
Fran <franencabo@gmail.com>
2022-01-30 17:13:38 +0100

    Modifico las funciones para hacer error-first

--- descuentos.js
+++ descuentos.js
@@ -1,28 +1,51 @@
 // Definimos las variables
 
-const precioOriginal = 137;
-const descuento = 18;
+const coupons = [
+  {
+    name: "Cupon10",
+    discount: 10,
+  },
+  {
+    name: "Cupon20",
+    discount: 20,
+  },
+  {
+    name: "Cupon25",
+    discount: 25,
+  },
+  {
+    name: "Cupon50",
+    discount: 50,
+  },
+];
 
-
-
-function calcularPrecioConDescuento(precio, descuento){
-    
-    const porcentajeACobrar = 100 - descuento;
-    const precioConDescuento = (precio * porcentajeACobrar)/100;
-    return precioConDescuento;
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajeACobrar = 100 - descuento;
+  const precioConDescuento = (precio * porcentajeACobrar) / 100;
+  return precioConDescuento;
 }
 
+function onclickCalcularDescuento() {
+  const inputPrice = document.getElementById("inputPrice");
+  const priceValue = inputPrice.value;
+  const inputCoupon = document.getElementById("inputCoupon");
+  const couponValue = inputCoupon.value;
 
+  const isCouponValueValid = function (coupon) {
+    return coupon.name === couponValue;
+  };
 
-function onclickCalcularDescuento(){
-    const inputPrice = document.getElementById("inputPrice");
-    const priceValue = inputPrice.value;
-    const inputDiscount = document.getElementById("inputDiscount");
-    const discountValue = inputDiscount.value;
+  const userCoupon = coupons.find(isCouponValueValid);
 
-    const calcularDescuento = calcularPrecioConDescuento(priceValue, discountValue);
- 
+  if (!userCoupon) {
+    alert("El cupón " + userCouponValue + " no es válido");
+  } else {
+    const descuento = userCoupon.discount;
+
+    const calcularDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
     const resultado = document.getElementById("resultado");
-    resultado.innerText = "El precio total con descuento es de " + calcularDescuento + "€";
+    resultado.innerText =
+      "El precio total con descuento es de " + calcularDescuento + "€";
+  }
 }
-
